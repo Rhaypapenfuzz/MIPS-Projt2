d26126758427ab059b7749e2a25dcac90ee70134
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-11-13 01:25:42 -0500

    Update project2.s
    Different instances of error messages to be printed and initializing .text

--- project2.s
+++ project2.s
@@ -2,3 +2,7 @@
 	char_array: .space 1000
 	#Invalid Messages
 	not_valid: .asciiz "Invalid base-31 number."
+	empty: .asciiz "Input is empty."
+	too_long: .asciiz "Input is too long."
+.text
+.globl main
