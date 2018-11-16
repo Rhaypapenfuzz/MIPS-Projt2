c6a7f6d2e4d6d695bca4a072d5452881b444b842
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-11-16 18:05:49 -0500

    Update project2.s
    Just an initialization of main and an additional error message

--- project2.s
+++ project2.s
@@ -3,6 +3,9 @@
 char_array:			.space 4000
 too_long:			.asciiz "Input is too long."
 not_valid:			.asciiz "Invalid base-31 number."
-	too_long: .asciiz "Input is too long."
-.text
+empty_string_error: .asciiz "Input is empty."
+
+.text           #Assembly language instruction
 .globl main
+
+main:
