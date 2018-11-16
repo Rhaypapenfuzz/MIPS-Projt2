22a17193dfb1b64a863a815754960623e82969fe
Raymond Papenfuss Gyabeng <39167251+Rhaypapenfuzz@users.noreply.github.com>
2018-11-16 18:04:03 -0500

    Update project2.s
    Initiailizing error msgs

--- project2.s
+++ project2.s
@@ -1,8 +1,8 @@
-.data
-	char_array: .space 1000
-	#Invalid Messages
-	not_valid: .asciiz "Invalid base-31 number."
-	empty: .asciiz "Input is empty."
+.data           #Data declaration section
+#Invalid Messages
+char_array:			.space 4000
+too_long:			.asciiz "Input is too long."
+not_valid:			.asciiz "Invalid base-31 number."
 	too_long: .asciiz "Input is too long."
 .text
 .globl main
